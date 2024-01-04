b4cce7294fee74a4c50248df9e846457b366d445
Austin Noon <123850831+AustinNoon@users.noreply.github.com>
2024-01-04 18:31:33 -0500

    Create pch.h
    precompiled header

---
+++ Space Attack/pch.h
@@ -0,0 +1,16 @@
+//standard C++ includes
+#include <ctime>
+#include "time.h"
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <string>
+#include <sstream>
+
+//sfml includes
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
